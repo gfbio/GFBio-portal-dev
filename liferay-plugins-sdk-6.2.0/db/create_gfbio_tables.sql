b449bdc23c5fe8dfcc533a5e6bc8b8668f59107f
Felicitas Loeffler <felicitas.loeffler@uni-jena.de>
2015-02-18 12:25:07 +0100

    external datasource added

---
+++ liferay-plugins-sdk-6.2.0/db/create_gfbio_tables.sql
@@ -0,0 +1,16 @@
+﻿-- Table: gfbio_project
+
+-- DROP TABLE gfbio_project;
+
+CREATE TABLE gfbio_project
+(
+  projectID bigint NOT NULL,
+  name varchar(50),
+  description text,
+  CONSTRAINT project_pkey PRIMARY KEY (projectID)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE gfbio_project
+  OWNER TO liferay_gfbio;
