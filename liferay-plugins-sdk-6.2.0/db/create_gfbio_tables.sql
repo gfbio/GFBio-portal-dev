de81754b4e22bb7782ecb2cc7e3faface7739c05
Ivaylo Kostadinov <ikostadi@mpi-bremen.de>
2015-11-13 17:02:39 +0100

    moved submission registry code back to its own file; modified researchobject FK; restored DEFAULT value of archive_pid; removed DEFAULT value of public_after; deleted submission_registry specific functions

--- liferay-plugins-sdk-6.2.0/db/create_gfbio_tables.sql
+++ liferay-plugins-sdk-6.2.0/db/create_gfbio_tables.sql
@@ -175,37 +175,10 @@ ALTER TABLE gfbio_researchobject
 
   
   
-------------------------------------- Submission Registry ------------------------------------- 
-  
+------------------------------------- Submission Registry -------------------------------------
 
-CREATE TABLE gfbio_submissionregistry
-(
-  researchobjectid bigint NOT NULL,
-  researchobjectversion smallint NOT NULL,
-  archive character(75) NOT NULL,
-  archive_pid text,
-  archive_pid_type bigint,
-  brokersubmissionid character(75)  NOT NULL,
-  last_changed timestamp with time zone NOT NULL DEFAULT now(),
-  userid bigint NOT NULL,
-  is_public boolean NOT NULL DEFAULT false,
-  public_after time with time zone DEFAULT (now() + '6 mons'::interval),
-  status text NOT NULL,
-  CONSTRAINT gfbio_submissionregistry_pkey PRIMARY KEY (researchobjectid, researchobjectversion, archive),
-  CONSTRAINT gfbio_dataprovider_fkey FOREIGN KEY (archive)
-      REFERENCES gfbio_dataprovider (label) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
- CONSTRAINT gfbio_researchobject_fkey FOREIGN KEY (researchobjectid, researchobjectversion)
-      REFERENCES gfbio_researchobject (researchobjectid, researchobjectversion) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE gfbio_submissionregistry
-  OWNER TO liferay_gfbio;  
-  
-  
+--- see submission_registry.sql
+
 ---------------------------------------------------------------------------------------------
 ------------------------------------ Other relationships ------------------------------------
 --------------------------------------      Tables     --------------------------------------
--- liferay-plugins-sdk-6.2.0/db/submission_registry/submission_registry.sql
+++ liferay-plugins-sdk-6.2.0/db/submission_registry/submission_registry.sql
@@ -1,37 +1,26 @@
-﻿-- DROP TABLE IF EXISTS gfbio_submission_registry;
-
-
-CREATE TYPE submission_status AS ENUM ('sent', 'archived');
-
---LOGIC: Contains one entry per research_object+archive combination.
-CREATE TABLE gfbio_submission_registry (
-research_object_id bigint NOT NULL REFERENCES gfbio_researchobject (researchobjectId),
-research_object_version smallint NOT NULL,
-archive text NOT NULL REFERENCES data_provider (label), --FOREIGN KEY
-archive_pid text DEFAULT NULL,
-archive_pid_type bigint DEFAULT NULL REFERENCES gfbio_content (contentid), --FOREIGN KEY
-broker_submission_id UUID NOT NULL,
---submission_data json NOT NULL, --NOT NULL
-last_changed timestamp NOT NULL DEFAULT now(),
-person smallint NOT NULL REFERENCES person (user_id) ON UPDATE CASCADE ON DELETE RESTRICT,
-status submission_status NOT NULL DEFAULT 'sent',
-is_public boolean NOT NULL DEFAULT FALSE,
-public_after timestamp DEFAULT NULL,
-PRIMARY KEY (research_object_id,research_object_version,archive),
-UNIQUE (research_object_id,archive,archive_pid)
+﻿CREATE TABLE gfbio_submissionregistry
+(
+  researchobjectid bigint NOT NULL,
+  researchobjectversion smallint NOT NULL,
+  archive character(75) NOT NULL,
+  archive_pid text DEFAULT NULL,
+  archive_pid_type bigint,
+  brokersubmissionid character(75) NOT NULL,
+  last_changed timestamp with time zone NOT NULL DEFAULT now(),
+  userid bigint NOT NULL,
+  is_public boolean NOT NULL DEFAULT false,
+  public_after time with time zone DEFAULT NULL,
+  status text NOT NULL,
+  CONSTRAINT gfbio_submissionregistry_pkey PRIMARY KEY (researchobjectid, researchobjectversion, archive),
+  CONSTRAINT gfbio_dataprovider_fkey FOREIGN KEY (archive)
+      REFERENCES gfbio_dataprovider (label) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT gfbio_researchobject_fkey FOREIGN KEY (researchobjectid)
+      REFERENCES gfbio_researchobject (researchobjectid) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
 );
-
-CREATE OR REPLACE VIEW latest_submissions AS
-SELECT * FROM (
-       SELECT *,
-       	      rank() OVER (PARTITION BY research_object_id,archive ORDER BY research_object_version DESC) AS pos
-       FROM gfbio_submission_registry) AS iq
-       WHERE pos=1
-;
-
-CREATE OR REPLACE FUNCTION get_submission_history(roid bigint) RETURNS SETOF gfbio_submission_registry AS $$
-   BEGIN
-       RETURN QUERY SELECT * FROM gfbio_submission_registry WHERE research_object_id=roid;
-       RETURN;
-   END;
-$$ LANGUAGE plpgsql;
+ALTER TABLE gfbio_submissionregistry
+  OWNER TO liferay_gfbio;  
