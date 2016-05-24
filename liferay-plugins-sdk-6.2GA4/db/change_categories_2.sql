1e4f8c0fb018ad3c7df337f6863285990d2892fe
Sven Bingert <sven.bingert@gwdg.de>
2016-05-24 10:38:40 +0200

    added additional sql script to reflect recent updates of the database

---
+++ liferay-plugins-sdk-6.2GA4/db/change_categories_2.sql
@@ -0,0 +1,352 @@
+
+  
+-------------------------------------------------------------------------------------------
+------------------------------------ Head, Cell & Content ---------------------------------
+--------------------------------------     category      -----------------------------------
+-------------------------------------------------------------------------------------------
+
+
+------------------------------------- Content Table Content -------------------------------------
+
+
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1492','1','1','1493','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1494','1','2','1493','Botany');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1495','1','3','1493','Botany');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1496','1','1','1497','1496');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1498','1','2','1497','Ecology ');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1499','1','3','1497','Ecology ');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1500','1','1','1501','1500');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1502','1','2','1501','Geoscience');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1503','1','3','1501','Geoscience');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1504','1','1','1505','1504');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1506','1','2','1505','Microbiology ');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1507','1','3','1505','Microbiology ');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1508','1','1','1509','1508');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1510','1','2','1509','Mycology');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1511','1','3','1509','Mycology');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1512','1','1','1513','1512');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1514','1','2','1513','Palaeontology');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1515','1','3','1513','Palaeontology');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1516','1','1','1517','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1518','1','2','1517','Zoology ');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1519','1','3','1517','Zoology ');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1520','1','1','1521','1520');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1522','1','2','1521','Other discipline');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1523','1','3','1521','Other discipline ');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1552','1','22','1493','');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1553','1','22','1496','');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1554','1','22','1500','');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1555','1','22','1504','');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1556','1','22','1508','');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1557','1','22','1512','');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1558','1','22','1516','');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1559','1','22','1520','');
+
+
+-------------------------------------------------------------------------------------------
+------------------------------------ Head, Cell & Content ---------------------------------
+--------------------------------------     Type      -----------------------------------
+-------------------------------------------------------------------------------------------
+
+
+------------------------------------- Content Table Content -------------------------------------
+
+
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1524','5','1','1525','1524');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1526','5','2','1525','research field');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1527','5','3','1525','research field');
+
+-------------------------------------------------------------------------------------------
+------------------------------------ Head, Cell & Content ---------------------------------
+-----------------------------------     category Type      --------------------------------
+-------------------------------------------------------------------------------------------
+
+
+------------------------------------- Content Table Content -------------------------------------
+
+
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1528','6','15','1529','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1530','6','16','1529','1524');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1531','6','15','1532','1496');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1533','6','16','1532','1524');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1534','6','15','1535','1500');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1536','6','16','1535','1524');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1537','6','15','1538','1504');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1539','6','16','1538','1524');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1540','6','15','1541','1508');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1542','6','16','1541','1524');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1543','6','15','1544','1512');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1545','6','16','1544','1524');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1546','6','15','1547','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1548','6','16','1547','1524');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1549','6','15','1550','1520');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1551','6','16','1550','1524');
+
+
+
+-------------------------------------------------------------------------------------------
+------------------------------------ Head, Cell & Content ---------------------------------
+----------------------     Delete old categories and researchfields     -------------------
+-------------------------------------------------------------------------------------------
+
+
+DELETE 
+FROM 
+	public.gfbio_content
+where
+	headid=2 or 
+	headid= 4 or
+	(
+		headid = 7 AND
+		cellcontent in 
+		(  
+			SELECT 
+				cast (rowid as text) 
+			FROM 
+				public.gfbio_content
+			WHERE
+				headid = 1 AND
+				rowid in
+				(
+					SELECT 
+						cast (cellcontent as bigint)
+					FROM 
+						public.gfbio_content
+					WHERE
+						columnid =15 AND
+						headid = 6 AND
+						rowid in
+						(
+							SELECT 
+								rowid
+							FROM 
+								public.gfbio_content
+							WHERE
+								headid = 6 AND
+								cellcontent in 
+								(
+									SELECT 
+									  cast (rowid AS text) 
+									FROM 
+									  public.gfbio_content
+									WHERE
+										headid = 5 AND
+										(
+											cellcontent ='accesscontrol' OR
+											cellcontent ='collection taxon' OR
+											cellcontent ='collection type' OR
+											cellcontent ='datadomain' OR
+											cellcontent ='ecological field' OR
+											cellcontent ='geographic region' OR
+											cellcontent ='geographic region' OR
+											cellcontent ='taxonomic field' 
+										)
+								)
+						)
+				)
+		)
+	)
+	or
+	(
+		headid = 1 AND
+		rowid in
+		(
+			SELECT 
+				cast (cellcontent as bigint)
+			FROM 
+				public.gfbio_content
+			WHERE
+				columnid =15 AND
+				headid = 6 AND
+				rowid in
+				(
+					SELECT 
+						rowid
+					FROM 
+						public.gfbio_content
+					WHERE
+						headid = 6 AND
+						cellcontent in 
+						(
+							SELECT 
+								cast (rowid AS text) 
+							FROM 
+								public.gfbio_content
+							WHERE
+								headid = 5 AND
+								(
+									cellcontent ='accesscontrol' OR
+									cellcontent ='collection taxon' OR
+									cellcontent ='collection type' OR
+									cellcontent ='datadomain' OR
+									cellcontent ='ecological field' OR
+									cellcontent ='geographic region' OR
+									cellcontent ='geographic region' OR
+									cellcontent ='taxonomic field' 
+								)
+						)
+				)
+		)
+	)
+	or
+	(
+		headid = 6 AND
+		rowid in(
+  SELECT 
+  rowid
+FROM 
+  public.gfbio_content
+WHERE
+  headid = 6 AND
+  cellcontent in (
+	SELECT 
+	  cast (rowid AS text) 
+	FROM 
+	  public.gfbio_content
+	WHERE
+	  headid = 5 AND
+	  (
+		cellcontent ='accesscontrol' OR
+		cellcontent ='collection taxon' OR
+		cellcontent ='collection type' OR
+		cellcontent ='datadomain' OR
+		cellcontent ='ecological field' OR
+		cellcontent ='geographic region' OR
+		cellcontent ='geographic region' OR
+		cellcontent ='taxonomic field' 
+	  )
+  )
+  )or
+  (
+  headid = 5 AND
+  rowid in (
+	SELECT 
+	  rowid
+	FROM 
+	  public.gfbio_content
+	WHERE
+	  headid = 5 AND
+	  (
+		cellcontent ='accesscontrol' OR
+		cellcontent ='collection taxon' OR
+		cellcontent ='collection type' OR
+		cellcontent ='datadomain' OR
+		cellcontent ='ecological field' OR
+		cellcontent ='geographic region' OR
+		cellcontent ='geographic region' OR
+		cellcontent ='taxonomic field' 
+	  )
+	  )
+  )
+  );
+
+
+Delete
+FROM 
+  public.gfbio_column
+WHERE
+  headid = 4 or
+  headid = 2;
+
+
+Delete
+FROM
+  public.gfbio_head
+WHERE
+  headid = 2 or 
+  headid =4;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+--------------------------------------------------------------------------------------------
+------------------------------------ Head, Cell & Content ----------------------------------
+--------------------------------     category dataprovider     -----------------------------
+--------------------------------------------------------------------------------------------
+
+
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1568','7','17','1569','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1570','7','18','1569','3');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1571','7','17','1572','1500');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1573','7','18','1572','3');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1574','7','17','1575','1512');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1576','7','18','1575','3');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1577','7','17','1578','1512');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1579','7','18','1578','3');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1580','7','17','1581','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1582','7','18','1581','3');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1583','7','17','1584','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1585','7','18','1584','4');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1586','7','17','1587','1508');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1588','7','18','1587','4');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1589','7','17','1590','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1591','7','18','1590','4');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1592','7','17','1593','1496');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1594','7','18','1593','4');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1595','7','17','1596','1500');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1597','7','18','1596','4');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1598','7','17','1599','1520');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1600','7','18','1599','4');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1601','7','17','1602','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1603','7','18','1602','5');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1604','7','17','1605','1512');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1606','7','18','1605','5');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1607','7','17','1608','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1609','7','18','1608','5');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1610','7','17','1611','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1612','7','18','1611','6');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1613','7','17','1614','1508');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1615','7','18','1614','6');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1616','7','17','1617','1512');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1618','7','18','1617','6');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1619','7','17','1620','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1621','7','18','1620','6');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1622','7','17','1623','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1624','7','18','1623','7');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1625','7','17','1626','1508');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1627','7','18','1626','7');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1628','7','17','1629','1512');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1630','7','18','1629','7');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1631','7','17','1632','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1633','7','18','1632','7');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1634','7','17','1635','1512');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1636','7','18','1635','8');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1637','7','17','1638','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1639','7','18','1638','8');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1640','7','17','1641','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1642','7','18','1641','9');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1643','7','17','1644','1496');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1645','7','18','1644','9');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1646','7','17','1647','1500');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1648','7','18','1647','9');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1649','7','17','1650','1504');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1651','7','18','1650','9');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1652','7','17','1653','1508');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1654','7','18','1653','9');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1655','7','17','1656','1512');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1657','7','18','1656','9');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1658','7','17','1659','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1660','7','18','1659','9');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1661','7','17','1662','1520');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1663','7','18','1662','9');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1664','7','17','1665','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1666','7','18','1665','1');  
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1667','7','17','1668','1496');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1669','7','18','1668','1');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1670','7','17','1671','1492');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1672','7','18','1671','2');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1673','7','17','1674','1504');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1675','7','18','1674','2');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1676','7','17','1677','1508');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1678','7','18','1677','2');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1679','7','17','1680','1516');
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1681','7','18','1680','2');
