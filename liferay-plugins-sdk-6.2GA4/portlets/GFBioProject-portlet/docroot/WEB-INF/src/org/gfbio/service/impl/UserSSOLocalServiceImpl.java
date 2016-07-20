13ff2d14dc56519bf8cc90c11cc0af72998c6deb
Kobkaew <kobkaew@gmail.com>
2016-07-20 14:26:01 +0200

    Add new table (User SSO) to store temporary token

---
+++ liferay-plugins-sdk-6.2GA4/portlets/GFBioProject-portlet/docroot/WEB-INF/src/org/gfbio/service/impl/UserSSOLocalServiceImpl.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.gfbio.service.impl;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.gfbio.NoSuchBasketException;
+import org.gfbio.NoSuchUserSSOException;
+import org.gfbio.model.Basket;
+import org.gfbio.model.UserSSO;
+import org.gfbio.service.base.UserSSOLocalServiceBaseImpl;
+
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.exception.SystemException;
+
+/**
+ * The implementation of the user s s o local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are
+ * added, rerun ServiceBuilder to copy their definitions into the
+ * {@link org.gfbio.service.UserSSOLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM.
+ * </p>
+ *
+ * @author Marcel Froemming
+ * @see org.gfbio.service.base.UserSSOLocalServiceBaseImpl
+ * @see org.gfbio.service.UserSSOLocalServiceUtil
+ */
+public class UserSSOLocalServiceImpl extends UserSSOLocalServiceBaseImpl {
+	public UserSSO updateUserSSO(long userID, String token) throws Exception {
+		UserSSO sso = null;
+		Date now = new Date();
+		try {
+			// update userSSO
+			sso = userSSOPersistence.findByPrimaryKey(userID);
+			sso.setUserID(userID);
+			sso.setToken(token);
+			sso.setLastModifiedDate(now);
+			super.updateUserSSO(sso);
+
+		} catch (NoSuchUserSSOException e) {
+			// create new userSSO
+			sso = userSSOPersistence.create(CounterLocalServiceUtil
+					.increment(getModelClassName()));
+			sso.setUserID(userID);
+			sso.setToken(token);
+			sso.setLastModifiedDate(now);
+			super.updateUserSSO(sso);
+			e.printStackTrace();
+		}
+
+		return sso;
+	}
+	
+	public boolean authorizeToken(String token, long userID) throws Exception {
+		boolean success = false;
+		UserSSO sso = userSSOPersistence.findByUserID(userID);
+		if (sso != null) {
+			Date tokenDate = sso.getLastModifiedDate();
+			// only for the valid token
+			if (getDifferentDates(tokenDate) < 7) {
+				String savedToken = sso.getToken();
+				if (savedToken.equals(token)) {
+					success = true;
+				}
+			}
+		}
+
+		return success;
+	}
+
+	public String getToken(long userID) throws Exception {
+		String token = "";
+		UserSSO sso = this.userSSOPersistence.findByUserID(userID);
+		if (sso != null) {
+			Date tokenDate = sso.getLastModifiedDate();
+			if (getDifferentDates(tokenDate) >= 7) {
+				token = generateNewToken();
+				// update token to db
+				updateUserSSO(userID,token);
+			} else {// use the existing one
+				token = sso.getToken();
+			}
+		} else {
+			token = generateNewToken();
+			// create new row in db
+			updateUserSSO(userID,token);
+		}
+		return token;
+	}
+
+	private String generateNewToken() {
+		SecureRandom random = new SecureRandom();
+		String token = new BigInteger(130, random).toString(32);
+		return token;
+	}
+
+	private long getDifferentDates(Date refDate) {
+		// check if the existing token is older than a week
+		Date now = getNow();
+		long diff = now.getTime() - refDate.getTime();
+		long diffDays = diff / (1000 * 60 * 60 * 24);
+		return diffDays;
+	}
+
+	private Date getNow() {
+		// 1) create a java calendar instance
+		Calendar calendar = Calendar.getInstance();
+		// 2) get a java.util.Date from the calendar instance.
+		// this date will represent the current instant, or "now".
+		Date now = calendar.getTime();
+		// 3) a java current time (now) instance
+		// Timestamp currentTimestamp = new java.sql.Timestamp(now.getTime());
+		return now;
+	}
+
+}
