4bea82dea98d12d7c1e110e55b1b80b03a66455a
marcel froemming <marcel.froemming@uni-jena.de>
2016-09-20 10:09:35 +0200

    small bug by category dataprovider

--- liferay-plugins-sdk-6.2GA4/db/release_new.sql
+++ liferay-plugins-sdk-6.2GA4/db/release_new.sql
@@ -9,3 +9,11 @@ INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUE
 INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1912','11','32' ,'1910','no metadata');
 INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1913','11','33' ,'1910','');
 INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent)VALUES('1914','11','34' ,'1910','');
+
+
+--------------------------------------------------------------------------------------------
+------------------------------------ Head, Cell & Content ----------------------------------
+------------------------------      category / dataprovider      -- 20.09.2016 -------------
+--------------------------------------------------------------------------------------------
+
+update gfbio_content set cellcontent = '1496' WHERE contentid = 1574;
