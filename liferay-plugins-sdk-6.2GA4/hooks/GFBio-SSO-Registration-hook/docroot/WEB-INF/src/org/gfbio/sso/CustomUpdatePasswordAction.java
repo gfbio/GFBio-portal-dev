996be1027ae2c02a4ad36ca58a9e953d2e68c6d6
Kobkaew <kobkaew.opasjumruskit@uni-jena.de>
2017-06-09 19:18:00 +0200

    Registration hook with workflow process.
    TODO: add user by admin

--- liferay-plugins-sdk-6.2GA4/hooks/GFBio-SSO-Registration-hook/docroot/WEB-INF/src/org/gfbio/sso/CustomUpdatePasswordAction.java
+++ liferay-plugins-sdk-6.2GA4/hooks/GFBio-SSO-Registration-hook/docroot/WEB-INF/src/org/gfbio/sso/CustomUpdatePasswordAction.java
@@ -1,6 +1,5 @@
 package org.gfbio.sso;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -8,14 +7,19 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Ticket;
 import com.liferay.portal.model.User;
+import com.liferay.portal.model.UserGroupRole;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.ServiceContextFactory;
+import com.liferay.portal.service.ServiceContextThreadLocal;
+import com.liferay.portal.service.ServiceContextUtil;
 import com.liferay.portal.service.UserLocalService;
-import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceWrapper;
-import com.liferay.portal.model.UserGroupRole;
+import com.liferay.portal.service.persistence.UserPersistence;
 
 public class CustomUpdatePasswordAction extends UserLocalServiceWrapper {
 
@@ -26,94 +30,33 @@ public class CustomUpdatePasswordAction extends UserLocalServiceWrapper {
 		LDAPUserAccount.init();
 	}
 
-	/*
-	 * @Override public User updateUser(long userId, String oldPassword, String
-	 * newPassword1, String newPassword2, boolean passwordReset, String
-	 * reminderQueryQuestion, String reminderQueryAnswer, String screenName,
-	 * String emailAddress, long facebookId, String openId, String languageId,
-	 * String timeZoneId, String greeting, String comments, String firstName,
-	 * String middleName, String lastName, int prefixId, int suffixId, boolean
-	 * male, int birthdayMonth, int birthdayDay, int birthdayYear, String smsSn,
-	 * String aimSn, String facebookSn, String icqSn, String jabberSn, String
-	 * msnSn, String mySpaceSn, String skypeSn, String twitterSn, String ymSn,
-	 * String jobTitle, long[] groupIds, long[] organizationIds, long[] roleIds,
-	 * List<UserGroupRole> userGroupRoles, long[] userGroupIds, ServiceContext
-	 * serviceContext) throws PortalException, SystemException { // called when
-	 * login, change password, reset password log.info(":: Hook on update user."
-	 * ); if (groupIds != null)log.info(":: groupIds:"
-	 * +Arrays.toString(groupIds)); if (organizationIds != null)log.info(
-	 * ":: organizationIds:"+Arrays.toString(organizationIds)); if (roleIds !=
-	 * null)log.info(":: roleIds:"+Arrays.toString(roleIds)); if (userGroupRoles
-	 * != null) log.info(":: userGroupRoles:"+ userGroupRoles.toString()); if
-	 * (userGroupIds !=null)log.info(":: userGroupIds:"
-	 * +Arrays.toString(userGroupIds));
-	 * 
-	 * 
-	 * User user = super.getUser(userId); if (emailAddress != null) log.info(
-	 * ":: emailAddress:"+emailAddress); try { // if (!isReservedEmail(user)) {
-	 * // If the email is used by the backend (e.g. Portal mail // settings),
-	 * the password should not be updated // } log.info(":: updateUser "
-	 * +userId); log.info(":: updateUser "+newPassword1); user =
-	 * super.updateUser(userId, oldPassword, newPassword1, newPassword2,
-	 * passwordReset, reminderQueryQuestion, reminderQueryAnswer, screenName,
-	 * emailAddress, facebookId, openId, languageId, timeZoneId, greeting,
-	 * comments, firstName, middleName, lastName, prefixId, suffixId, male,
-	 * birthdayMonth, birthdayDay, birthdayYear, smsSn, aimSn, facebookSn,
-	 * icqSn, jabberSn, msnSn, mySpaceSn, skypeSn, twitterSn, ymSn, jobTitle,
-	 * groupIds, organizationIds, roleIds, userGroupRoles, userGroupIds,
-	 * serviceContext); if (newPassword1.trim() != "" && newPassword1 != null) {
-	 * updatePassword(userId, newPassword1,newPassword2, false); log.info(
-	 * ":: updatePassword"); } } catch (Exception e) { e.printStackTrace();
-	 * log.error(e.toString()); return null; } return user; }
-	 */
-
-	@Override
-	public void sendEmailAddressVerification(
-			User user, String emailAddress, ServiceContext serviceContext)
-					throws PortalException {
-
-		log.info(":: Hook on sendEmailAddressVerification.");
-		try {
-			
-			String password = (String)serviceContext.getAttribute(
-						"passwordUnencrypted");
-			
-			/*boolean isLDAPUserAdded = LDAPUserAccount.LDAPaddUser(user, password);
-			
-			/*if (isLDAPUserAdded) { // create JIRA account
-				log.info(":: LDAP user successfully added");
-				URLBasicAuth.urlAuth(emailAddress, password);
-			}*/
-			
-			super.completeUserRegistration(user, serviceContext);
-			log.info(":: emailAddress:" + emailAddress);
-			log.info(":: password:" + password);
-		} catch (Exception e) {
-			e.printStackTrace();
-			log.error(e.toString());
-		}
-	}
-
-	@Override
-	public User addUser(long creatorUserId, long companyId, boolean autoPassword, String password1, String password2,
-			boolean autoScreenName, String screenName, String emailAddress, long facebookId, String openId,
-			Locale locale, String firstName, String middleName, String lastName, int prefixId, int suffixId,
-			boolean male, int birthdayMonth, int birthdayDay, int birthdayYear, String jobTitle, long[] groupIds,
-			long[] organizationIds, long[] roleIds, long[] userGroupIds, boolean sendEmail,
+	@Override // add user with email-validation
+	public User addUserWithWorkflow(long creatorUserId, long companyId, boolean autoPassword, String password1,
+			String password2, boolean autoScreenName, String screenName, String emailAddress, long facebookId,
+			String openId, Locale locale, String firstName, String middleName, String lastName, int prefixId,
+			int suffixId, boolean male, int birthdayMonth, int birthdayDay, int birthdayYear, String jobTitle,
+			long[] groupIds, long[] organizationIds, long[] roleIds, long[] userGroupIds, boolean sendEmail,
 			ServiceContext serviceContext) throws PortalException, SystemException {
 
-		log.info(":: Hook on add user.");
-		try {
+		log.info(":: Hook on addUserWithWorkflow.");
+		try {		
+			User user = super.addUserWithWorkflow(creatorUserId, companyId, autoPassword, password1, password2,
+					autoScreenName, screenName, emailAddress, facebookId, openId, locale, firstName, middleName,
+					lastName, prefixId, suffixId, male, birthdayMonth, birthdayDay, birthdayYear, jobTitle, groupIds,
+					organizationIds, roleIds, userGroupIds, sendEmail, serviceContext);
+			log.info(":: creatorUserId "+creatorUserId);
+			if (creatorUserId == super.getDefaultUserId(companyId)) {
+				boolean isUserAdded = LDAPUserAccount.LDAPaddUser(user, password1);
 
-			User user = super.addUser(creatorUserId, companyId, autoPassword, password1, password2, autoScreenName,
-					screenName, emailAddress, facebookId, openId, locale, firstName, middleName, lastName, prefixId,
-					suffixId, male, birthdayMonth, birthdayDay, birthdayYear, jobTitle, groupIds, organizationIds,
-					roleIds, userGroupIds, sendEmail, serviceContext);
-
-			boolean isUserAdded = LDAPUserAccount.LDAPaddUser(user, password1);
-			if (isUserAdded) { // create JIRA account
-				log.info(":: LDAP user successfully added");
-				URLBasicAuth.urlAuth(user.getEmailAddress(), password1);
+				if (isUserAdded) { // create JIRA account
+					log.info(":: LDAP user successfully added");
+					URLBasicAuth.urlAuth(user.getEmailAddress(), password1);
+				}
+			}else{
+				log.info(":: added a user by admin.");
+				//TODO: add LDAP user, JIRA account
+				//TODO: send a password reset link
+//				log.info(user.getPassword());
 			}
 			return user;
 		} catch (Exception e) {
@@ -123,41 +66,78 @@ public class CustomUpdatePasswordAction extends UserLocalServiceWrapper {
 		}
 	}
 
+	/*
+	 * @Override public User updatePassword(long userId, String password1,
+	 * String password2, boolean passwordReset) throws PortalException,
+	 * SystemException { // called when creating a new account, updateUser(hook)
+	 * log.info(":: Hook on updatePassword 1."); User user =
+	 * super.updatePassword(userId, password1, password2, passwordReset); try {
+	 * // log.info(":: "+password1); // boolean isUserAdded =
+	 * LDAPUserAccount.LDAPaddUser(user, password1); // if (isUserAdded) { //
+	 * log.info(":: LDAP user successfully added"); //
+	 * URLBasicAuth.urlAuth(user.getEmailAddress(), password1); // } } catch
+	 * (Exception e) { e.printStackTrace(); log.error(e.toString()); } return
+	 * user; }
+	 */
 	@Override
-	public User updatePassword(long userId, String password1, String password2, boolean passwordReset)
-			throws PortalException, SystemException {
+	public User updatePassword(long userId, String password1, String password2, boolean passwordReset,
+			boolean silentUpdate) throws PortalException, SystemException {
 		// called when creating a new account, updateUser(hook)
-		log.info(":: Hook on updatePassword.");
-		User user = super.updatePassword(userId, password1, password2, passwordReset);
-		try {
-			boolean isUserAdded = LDAPUserAccount.LDAPaddUser(user, password1);
-			if (isUserAdded) {
-				log.info(":: LDAP user successfully added");
-				URLBasicAuth.urlAuth(user.getEmailAddress(), password1);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			log.error(e.toString());
+		User user = super.getUser(userId);
+		boolean isVerified = user.getEmailAddressVerified();
+		if (isVerified) {
+			user = super.updatePassword(userId, password1, password2, passwordReset, silentUpdate);
+		} else {// prevent re-sending a verification email after adding LDAP
+				// user
+			log.warn(":: no password update - unverified user");
 		}
 		return user;
 	}
 
 	@Override
-	public User updateEmailAddress(long userId, String password, String emailAddress1, String emailAddress2)
+	public User updateUser(long userId, String oldPassword, String newPassword1, String newPassword2,
+			boolean passwordReset, String reminderQueryQuestion, String reminderQueryAnswer, String screenName,
+			String emailAddress, long facebookId, String openId, String languageId, String timeZoneId, String greeting,
+			String comments, String firstName, String middleName, String lastName, int prefixId, int suffixId,
+			boolean male, int birthdayMonth, int birthdayDay, int birthdayYear, String smsSn, String aimSn,
+			String facebookSn, String icqSn, String jabberSn, String msnSn, String mySpaceSn, String skypeSn,
+			String twitterSn, String ymSn, String jobTitle, long[] groupIds, long[] organizationIds, long[] roleIds,
+			List<UserGroupRole> userGroupRoles, long[] userGroupIds, ServiceContext serviceContext)
 			throws PortalException, SystemException {
-		log.info(":: Hook on updateEmailAddress.");
-		User user = super.updateEmailAddress(userId, password, emailAddress1, emailAddress2);
+
+		log.info(":: Hook on updateUser.");
 		try {
-			boolean isUserAdded = LDAPUserAccount.LDAPaddUser(user, password);
-			if (isUserAdded) {
-				log.info(":: LDAP user successfully added");
-				URLBasicAuth.urlAuth(user.getEmailAddress(), password);
+			User user = super.getUser(userId);
+			if (newPassword1.length() > 0) { // from setting new password
+				log.info(":: change password");
+				log.info(":: " + newPassword1);
+				// update LDAP user without connecting to JIRA
+				LDAPUserAccount.LDAPaddUser(user, newPassword1);
+			} /*else { // no new password is set
+				if (oldPassword.length() > 0) { // add new user
+					log.info(":: add new user");
+				} else {
+					log.info(":: update user without changing password");
+				}
+			}*/
+			boolean isVerified = user.getEmailAddressVerified();
+			if (isVerified) {
+				user = super.updateUser(userId, oldPassword, newPassword1, newPassword2, passwordReset,
+						reminderQueryQuestion, reminderQueryAnswer, screenName, emailAddress, facebookId, openId,
+						languageId, timeZoneId, greeting, comments, firstName, middleName, lastName, prefixId, suffixId,
+						male, birthdayMonth, birthdayDay, birthdayYear, smsSn, aimSn, facebookSn, icqSn, jabberSn,
+						msnSn, mySpaceSn, skypeSn, twitterSn, ymSn, jobTitle, groupIds, organizationIds, roleIds,
+						userGroupRoles, userGroupIds, serviceContext);
+			} else { // prevent re-sending a verification email after adding
+						// LDAP user
+				log.warn(":: no user update - unverified user");
 			}
+			return user;
 		} catch (Exception e) {
 			e.printStackTrace();
 			log.error(e.toString());
+			return null;
 		}
-		return user;
 	}
 
 }
