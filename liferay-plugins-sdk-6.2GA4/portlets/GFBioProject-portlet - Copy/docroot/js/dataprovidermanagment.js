dcc7f8125d74b7de5a5a2b4d9e38ef1556659c6d
dina.sharafeldeen <dina.sharafeldeen@uni-jena.de>
2017-08-16 11:08:19 +0200

    delete copy folder

--- liferay-plugins-sdk-6.2GA4/portlets/GFBioProject-portlet - Copy/docroot/js/dataprovidermanagment.js
+++
@@ -1,9 +0,0 @@
-
-
-//
-function choosedpmaDataProvider(method, label  ,divId){
-	dataProviderManagmentURL = document.getElementById('dataprovidermanagmenturl').value;
-	var data = {};
-	data["label"] = label;
-	ajaxActionRequest_choosedpmaDataProvider(dataProviderManagmentURL, method, data, divId,false);
-}
