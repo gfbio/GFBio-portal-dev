4aad5dd4e1b21051e97a3d8a1ed9d78c1b7e2a2c
marcel froemming <marcel.froemming@uni-jena.de>
2017-09-29 10:30:31 +0200

    extract 'url' from extended data in gfbio_license

--- liferay-plugins-sdk-6.2GA4/db/release_new.sql
+++ liferay-plugins-sdk-6.2GA4/db/release_new.sql
@@ -3,3 +3,22 @@
 ------------------------------------------------------------------------------------------------
 
 ALTER TABLE gfbio_researchobject ALTER COLUMN extendeddata  TYPE text;
+
+------------------------------------------------------------------------------------------------
+--------------------------------------       license        -- 04.08.2017 ----------------------
+------------------------------------------------------------------------------------------------
+
+
+INSERT INTO gfbio_column (columnid, headid, column_name, lastmodifieddate)VALUES(getNewColumnId(),(select getHeadIdByName('gfbio_license')),'url', now());
+
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent, lastmodifieddate)VALUES(getNewContentId(),(select getHeadIdByName('gfbio_license')), getColumnIdByNames('gfbio_license','url'), getrowIdByContentId(getContentIdByNames('gfbio_license', 'label', 'CC BY')), 'https://creativecommons.org/licenses/by/3.0/de', now());
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent, lastmodifieddate)VALUES(getNewContentId(),(select getHeadIdByName('gfbio_license')), getColumnIdByNames('gfbio_license','url'), getrowIdByContentId(getContentIdByNames('gfbio_license', 'label', 'CC BY-SA')), '', now());
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent, lastmodifieddate)VALUES(getNewContentId(),(select getHeadIdByName('gfbio_license')), getColumnIdByNames('gfbio_license','url'), getrowIdByContentId(getContentIdByNames('gfbio_license', 'label', 'CC BY-NC-SA')), 'https://creativecommons.org/licenses/by-sa/3.0/de', now());
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent, lastmodifieddate)VALUES(getNewContentId(),(select getHeadIdByName('gfbio_license')), getColumnIdByNames('gfbio_license','url'), getrowIdByContentId(getContentIdByNames('gfbio_license', 'label', 'CC BY-NC')), '', now());
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent, lastmodifieddate)VALUES(getNewContentId(),(select getHeadIdByName('gfbio_license')), getColumnIdByNames('gfbio_license','url'), getrowIdByContentId(getContentIdByNames('gfbio_license', 'label', 'CC BY-NC-ND')), '', now());
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent, lastmodifieddate)VALUES(getNewContentId(),(select getHeadIdByName('gfbio_license')), getColumnIdByNames('gfbio_license','url'), getrowIdByContentId(getContentIdByNames('gfbio_license', 'label', 'CC BY-ND')), '', now());
+INSERT INTO gfbio_content (contentid, headid, columnid, rowid, cellcontent, lastmodifieddate)VALUES(getNewContentId(),(select getHeadIdByName('gfbio_license')), getColumnIdByNames('gfbio_license','url'), getrowIdByContentId(getContentIdByNames('gfbio_license', 'label', 'CC0')), 'https://creativecommons.org/publicdomain/zero/1.0/deed.de', now());
+
+SELECT changeCellContentByNames('gfbio_license', 'extendeddata', '{"url":"https://creativecommons.org/publicdomain/zero/1.0/deed.de"}', '');
+SELECT changeCellContentByNames('gfbio_license', 'extendeddata', '{"url":"https://creativecommons.org/licenses/by/3.0/de"}', '');
+SELECT changeCellContentByNames('gfbio_license', 'extendeddata', '{"url":"https://creativecommons.org/licenses/by-sa/3.0/de"}', '');
