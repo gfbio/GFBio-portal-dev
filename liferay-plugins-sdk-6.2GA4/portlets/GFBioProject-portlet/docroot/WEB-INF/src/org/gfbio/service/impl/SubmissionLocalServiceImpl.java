286293f472561188cc37bfb512baf39f6fe785bb
marcel froemming <marcel.froemming@uni-jena.de>
2018-02-16 15:07:00 +0100

    comment code, that was not important for dev (it is code for feature submission branch)

--- liferay-plugins-sdk-6.2GA4/portlets/GFBioProject-portlet/docroot/WEB-INF/src/org/gfbio/service/impl/SubmissionLocalServiceImpl.java
+++ liferay-plugins-sdk-6.2GA4/portlets/GFBioProject-portlet/docroot/WEB-INF/src/org/gfbio/service/impl/SubmissionLocalServiceImpl.java
@@ -30,8 +30,8 @@ import java.util.UUID;
 import org.apache.commons.codec.binary.Base64;
 import org.gfbio.helper.ContactJira;
 import org.gfbio.helper.Helper;
-import org.gfbio.helper.RuntimeExec;
-import org.gfbio.helper.StreamWrapper;
+//import org.gfbio.helper.RuntimeExec;
+//import org.gfbio.helper.StreamWrapper;
 import org.gfbio.idmg.jiraclient.model.Project;
 import org.gfbio.model.Submission;
 import org.gfbio.service.DataProviderLocalServiceUtil;
@@ -928,7 +928,7 @@ public class SubmissionLocalServiceImpl extends SubmissionLocalServiceBaseImpl {
 		return keyJson;
 	}
 	
-	public JSONObject startSubmission (JSONObject requestJson){
+/*	public JSONObject startSubmission (JSONObject requestJson){
 		JSONObject responseJson = new JSONObject();
 		
 		JSONObject checkJson = transferSubmissionInformationToDb(requestJson);
@@ -953,10 +953,10 @@ public class SubmissionLocalServiceImpl extends SubmissionLocalServiceBaseImpl {
 		
 		
 		return responseJson;
-	}
+	}*/
 	
 	
-	//
+/*	//
 	@SuppressWarnings({ "unchecked"})
 	public JSONObject transferSubmissionInformationToDb(JSONObject requestJson){
 		
@@ -1088,7 +1088,7 @@ public class SubmissionLocalServiceImpl extends SubmissionLocalServiceBaseImpl {
 						responseJson.put("project", projectJson);
 				}
 				
-/*				//primarydata
+				//primarydata
 				if (requestJson.containsKey("primarydata")){
 					JSONObject primarydataJson = Helper.getJsonObjectFromJson(requestJson, "primarydata");
 					
@@ -1104,7 +1104,7 @@ public class SubmissionLocalServiceImpl extends SubmissionLocalServiceBaseImpl {
 					}
 					responseJson.put("primarydata", primarydataJson);
 					
-				}*/
+				}
 				
 				//answer
 				JSONObject questionJson = new JSONObject ();
@@ -1123,7 +1123,7 @@ public class SubmissionLocalServiceImpl extends SubmissionLocalServiceBaseImpl {
 		
 		return responseJson;
 	}
-	
+	*/
 	
 	//
 	@SuppressWarnings("unchecked")
