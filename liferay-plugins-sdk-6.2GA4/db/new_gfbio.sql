72a9359d6c4554e2842c0ba295db3875a0e3cfd9
FelicitasLoeffler <felicitas.loeffler@uni-jena.de>
2020-02-17 09:52:17 +0100

    db change, long to VARCHAR goesternID

--- liferay-plugins-sdk-6.2GA4/db/new_gfbio.sql
+++ liferay-plugins-sdk-6.2GA4/db/new_gfbio.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS gfbio_usergoesternid;
 CREATE TABLE gfbio_usergoesternid
 (
   userid bigint NOT NULL,
-  goesternid bigint NOT NULL,
+  goesternid VARCHAR(255) NOT NULL,
   lastmodifieddate timestamp without time zone,
   CONSTRAINT gfbio_usergoesternid_pkey PRIMARY KEY (userid, goesternid)
 )
